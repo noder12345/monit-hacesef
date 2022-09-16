ae018cbfd873cebd5c6436de2a4cc60836be21ed
noder12345 <113690020+noder12345@users.noreply.github.com>
2022-09-16 15:52:00 +0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,147 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "באילו שנים חיו הלל ושמאי",
+    answer: "דור חמישי ואחרון של תקופת הזוגות",
+    options: [
+      "דור שלישי של תקופת הזוגות",
+      "דור חמישי ואחרון של תקופת הזוגות",
+      "לפני תקופת הזוגות",
+      "אחרי תקופת הזוגות"
+    ]
+  },
+  
+  {
+    numb: 2,
+    question: "מילה נרדפת לחכם בתנ'ך",
+    answer: "זקן",
+    options: [
+      "תחמן",
+      "פזיז",
+      "רציני",
+      "זקן"
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "כמה מצוות יש בתנ'ך",
+    answer: "613",
+    options: [
+      "613",
+      "282",
+      "5",
+      "24"
+    ]
+  },
+  
+  {
+    numb: 4,
+    question: "כמה ספרים יש בתנ'ך",
+    answer: "24",
+    options: [
+      "14",
+      "5",
+      "24",
+      "38"
+    ]
+  },
+  
+  {
+    numb: 5,
+    question: " 'אם אין אני לי מי לי' מי אמר ",
+    answer: "הלל",
+    options: [
+      "עומר אדם",
+      "שמואל",
+      "שמאי",
+      "הלל"
+    ]
+  },
+
+
+
+    {
+      numb: 6,
+      question: "מזה בו'ה",
+     answer: "ברכה והצלחה",
+      options: [
+       "בועה",
+       "ברכו והנה",
+        "ברכה והצלחה",
+      "בעזרה והצלחה"
+      ]
+    },
+
+
+    {
+      numb: 7,
+      question: "כמה תפילות היה צריך משה להתפלל",
+     answer: "515",
+      options: [
+       "365",
+       "515",
+        "613",
+      "201"
+      ]
+    },
+    
+    { 
+      numb: 8,
+        question: "מזה בס'ד",
+       answer: "בסיעתא דשמיא",
+       options: [
+         "בסיעתא דשמיא",
+         "באסתן דשלועא",
+          "באחן דשמין",
+       "באס דשרין"
+       ] 
+      },
+
+      {
+        numb: 9,
+               question: "כינוים ל'הימים הנוראיים'",
+              answer: "עשרת ימי תשובה",
+               options: [
+              "חודש הרחמים",
+              "עשרת ימי תשובה",
+              "גמר חתימה טובה",
+               "יום כיפור"
+               ]
+             },
+            
+            
+             {
+             numb: 10,
+             question: "כמה חומשי תורה יש",
+            answer: "חמישה",
+             options: [
+            "חמש מאות",
+            "עשר",
+            "חמישים",
+             "חמישה"
+             ]
+           },
+             
+
+
+
+]
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+  //{
+
+ 
